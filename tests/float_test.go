da8478a29b1033b7c80d6604ce4cb803bb5248f2
Dale McDiarmid <dale@clickhouse.com>
2022-05-24 10:51:08 +0100

    benchmark float

--- tests/float_test.go
+++ tests/float_test.go
@@ -46,3 +46,44 @@ func TestSimpleFloat(t *testing.T) {
 		}
 	}
 }
+
+func BenchmarkFloat(b *testing.B) {
+	var (
+		ctx       = context.Background()
+		conn, err = clickhouse.Open(&clickhouse.Options{
+			Addr: []string{"127.0.0.1:9000"},
+			Auth: clickhouse.Auth{
+				Database: "default",
+				Username: "default",
+				Password: "",
+			},
+		})
+	)
+	if err != nil {
+		b.Fatal(err)
+	}
+	defer func() {
+		conn.Exec(ctx, "DROP TABLE benchmark_float")
+	}()
+
+	if err = conn.Exec(ctx, `CREATE TABLE benchmark_float (Col1 Float32, Col2 Float64) ENGINE = Null`); err != nil {
+		b.Fatal(err)
+	}
+
+	const rowsInBlock = 10_000_000
+
+	for n := 0; n < b.N; n++ {
+		batch, err := conn.PrepareBatch(ctx, "INSERT INTO benchmark_float VALUES")
+		if err != nil {
+			b.Fatal(err)
+		}
+		for i := 0; i < rowsInBlock; i++ {
+			if err := batch.Append(float32(122.112), 322.111); err != nil {
+				b.Fatal(err)
+			}
+		}
+		if err = batch.Send(); err != nil {
+			b.Fatal(err)
+		}
+	}
+}
